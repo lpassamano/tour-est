cf5d7bf0617f01b93bd7bfb49c57ff10ce009dbb
Leigh Passamano <leighayna.josele@gmail.com>
2018-07-18 15:46:18 -0400

    Added additional tests for session controller

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,12 +2,6 @@ require 'rails_helper'
 
 RSpec.describe SessionsController do
   describe "post create" do
-    it 'is successful' do
-      # update for 401 status for failure
-      post :create
-      expect(response).to_not be_successful
-    end
-
     it 'provides a token when given a username and password' do
       user = create :staff_user
       post :create, params: {username: user.username, password: "password123"}
@@ -15,6 +9,31 @@ RSpec.describe SessionsController do
       expect(JSON.parse(response.body)).to eq("token" => "abc123")
     end
 
-    # other tests, no password, bad password, bad username 
+    it 'is not successful if no username and password is provided' do
+      post :create
+      expect(response.status).to eq(401)
+    end
+
+    it 'is not successful if no password is provided' do
+      user = create :staff_user
+      post :create, params: {username: user.username}
+      expect(response.status).to eq(401)
+      expect(JSON.parse(response.body)).to include("error")
+    end
+
+    it 'is not successful if an incorrect password is provided' do
+      user = create :staff_user
+      post :create, params: {username: user.username, password: "abc123"}
+      expect(response.status).to eq(401)
+      expect(JSON.parse(response.body)).to include("error")
+    end
+
+    it 'is not successful if an incorrect username is provided' do
+      user = create :staff_user
+      name = user.username + "1"
+      post :create, params: {username: name, password: "password123"}
+      expect(response.status).to eq(401)
+      expect(JSON.parse(response.body)).to include("error")
+    end
   end
 end
