58cf09a956e8217b5d015a6ef0a3ffc6c14a55f1
Leigh Passamano <leighayna.josele@gmail.com>
2018-07-20 10:27:33 -0400

    Add React

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+  def create
+    username = params[:username]
+    password = params[:password]
+
+    user = StaffUser.find_by(username: username)
+    if user&.authenticate(password)
+      token = Token.encode(user)
+      render json: { token: token }
+    else
+      render json: { error: "Unauthorized" }, status: 401
+    end
+  end
+end
