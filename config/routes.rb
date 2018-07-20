40c6c4ca5c38d9cedb253b8b0f73fa0205647af5
Leigh Passamano <leighayna.josele@gmail.com>
2018-07-20 13:29:44 -0400

    Set up a catch all route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  # redirects all routes to / if user requests html
+  get "*path", to: "pages#index", constraints: -> (request) {request.format.html?}
   resources :points
   resources :tours
   resources :staff_users
   resources :cultural_centers
   resources :sessions, only: [:create, :delete]
+  root to: "pages#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
