c4509b63ebc9f55eefbc241fdb6e9bbf41ac1431
Leigh Passamano <leighayna.josele@gmail.com>
2018-07-25 11:32:04 -0400

    Add StaffUserDashboard component

---
+++ app/javascript/packs/components/StaffUserDashboard.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+const StaffUserDashboard = ({ current_staff_user }) => (
+  <div>
+    Staff Dashboard
+  </div>
+);
+
+export default StaffUserDashboard;
