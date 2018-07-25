445a6bbc314668a3a9975f7d98a3f987e0261a59
Leigh Passamano <leighayna.josele@gmail.com>
2018-07-25 11:06:00 -0400

    Add App component to application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,13 @@
 import 'babel-polyfill'
 import React from 'react'
 import ReactDOM from 'react-dom'
+
 import LoginForm from './components/LoginForm'
+import App from './components/App'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <LoginForm />,
+    <App />,
     document.body.appendChild(document.createElement('div')),
   )
 })
