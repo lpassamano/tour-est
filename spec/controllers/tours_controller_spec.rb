c17a6cdb09e5d2a9a9bd927edc7868aa690ced20
Leigh Passamano <leighayna.josele@gmail.com>
2018-08-06 14:31:09 -0400

    Add tour title validation and additional tests

--- spec/controllers/tours_controller_spec.rb
+++ spec/controllers/tours_controller_spec.rb
@@ -17,14 +17,36 @@ RSpec.describe ToursController, type: :controller do
   end
 
   it 'can only be created if a staff user is logged in' do
-
+    # how to test? dashboard is only accessible when logged in...
   end
 
   it 'new tours are associated with the current staff user and their cultural center' do
+    user = create :staff_user
+    post :create, params: {
+      tour: {
+        title: "Really cool tour",
+        staff_user_id: user.id,
+        cultural_center_id: user.cultural_center.id
+      }
+    }
+    tour = Tour.all.last
 
+    expect(tour.staff_user).to eq(user)
+    expect(tour.cultural_center).to eq(user.cultural_center)
   end
 
   it 'tour is not created if no title is given' do
-
+    count1 = Tour.all.count
+    user = create :staff_user
+    post :create, params: {
+      tour: {
+        title: "",
+        staff_user_id: user.id,
+        cultural_center_id: user.cultural_center.id
+      }
+    }
+    count2 = Tour.all.count
+    
+    expect(count1).to eq(count2)
   end
 end
