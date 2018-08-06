365de1d86f6c1216be9a4b4d3155e8607b41afb6
Leigh Passamano <leighayna.josele@gmail.com>
2018-08-06 10:02:08 -0400

    Add tour factory

--- spec/models/tour_spec.rb
+++ spec/models/tour_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Tour, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a working factory' do
+    tour = create :tour
+    tour2 = create :tour
+
+    expect(tour.title).to_not eq(tour2.title)
+  end
+
+  context "Create Tour" do
+    
+  end
 end
