8530e005f9f2409a3861a8cf870723c0c61f6a66
Leigh Passamano <leighayna.josele@gmail.com>
2018-08-07 11:39:16 -0400

    Refactor tour controller spec

--- spec/controllers/tours_controller_spec.rb
+++ spec/controllers/tours_controller_spec.rb
@@ -3,50 +3,53 @@ require 'rails_helper'
 RSpec.describe ToursController, type: :controller do
   let(:user) { create :staff_user }
 
-  it 'creates a tour when provided a title' do
-    authenticate_staff_user(user)
-    post :create, params: {
-      tour: {
-        title: "Awesome Tour",
-        staff_user_id: user.id,
-        cultural_center_id: user.cultural_center.id
-      }
-    }
-    tour = Tour.all.last
+  context "User authenticated" do
+    before(:each) do
+      authenticate_staff_user(user)
+    end
 
-    expect(response.status).to eq(200)
-    expect(tour.title).to eq("Awesome Tour")
+    it 'creates a tour associated with the current user and their cultural center when provided valid data' do
+      post :create, params: {
+        tour: {
+          title: "Awesome Tour",
+          staff_user_id: user.id,
+          cultural_center_id: user.cultural_center.id
+        }
+      }
+      tour = Tour.all.last
+
+      expect(response.status).to eq(200)
+      expect(tour.title).to eq("Awesome Tour")
+      expect(tour.staff_user).to eq(user)
+      expect(tour.cultural_center).to eq(user.cultural_center)
+    end
+
+    it 'does not create a tour when provided invalid data' do
+      expect {
+        post :create, params: {
+          tour: {
+            title: "",
+            staff_user_id: user.id,
+            cultural_center_id: user.cultural_center.id
+          }
+        }
+      }.to_not change(Tour, :count)
+      expect(response.status).to eq(422)
+    end
   end
 
-  it 'can only be created if a staff user is logged in' do
-    post :create, params: {
-      tour: {
-        title: "Awesome Tour",
-        staff_user_id: "1",
-        cultural_center_id: "1"
-      }
-    }
-
-    expect(response.status).to eq(401)
+  context "User not authenticated" do
+    it 'tour can only be created if a staff user is logged in' do
+      expect {
+        post :create, params: {
+          tour: {
+            title: "Awesome Tour",
+            staff_user_id: user.id,
+            cultural_center_id: user.cultural_center.id
+          }
+        }
+      }.to_not change(Tour, :count)
+      expect(response.status).to eq(401)
+    end
   end
-
-  it 'new tours are associated with the current staff user and their cultural center' do
-    authenticate_staff_user(user)
-    post :create, params: {
-      tour: {
-        title: "Really cool tour",
-        staff_user_id: user.id,
-        cultural_center_id: user.cultural_center.id
-      }
-    }
-    tour = Tour.all.last
-
-    expect(response.status).to eq(200)
-    expect(tour.staff_user).to eq(user)
-    expect(tour.cultural_center).to eq(user.cultural_center)
-  end
-
-  # make 1 validation controller test
-  # make 2 contexts, authenticated and not
-
 end
