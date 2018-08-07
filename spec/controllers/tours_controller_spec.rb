8f0076251d9864a6ff806c408f5268a4cef1d3de
Leigh Passamano <leighayna.josele@gmail.com>
2018-08-07 09:58:53 -0400

    Add additional tests

--- spec/controllers/tours_controller_spec.rb
+++ spec/controllers/tours_controller_spec.rb
@@ -18,6 +18,8 @@ RSpec.describe ToursController, type: :controller do
 
   it 'can only be created if a staff user is logged in' do
     # how to test? dashboard is only accessible when logged in...
+    # since it gets the staff user/cc info from the dashboard, can test to make
+    # sure not created w/out a user or cc
   end
 
   it 'new tours are associated with the current staff user and their cultural center' do
@@ -46,7 +48,36 @@ RSpec.describe ToursController, type: :controller do
       }
     }
     count2 = Tour.all.count
-    
+
+    expect(count1).to eq(count2)
+  end
+
+  it 'tour is not created if it is not associated with a user' do
+    count1 = Tour.all.count
+    post :create, params: {
+      tour: {
+        title: "Cool Tour",
+        staff_user_id: "",
+        cultural_center_id: "1"
+      }
+    }
+    count2 = Tour.all.count
+
+    expect(count1).to eq(count2)
+  end
+
+  it 'tour is not created if it is not associated with a cultural center' do
+    count1 = Tour.all.count
+    user = create :staff_user
+    post :create, params: {
+      tour: {
+        title: "Cool Tour",
+        staff_user_id: user.id,
+        cultural_center_id: ""
+      }
+    }
+    count2 = Tour.all.count
+
     expect(count1).to eq(count2)
   end
 end
