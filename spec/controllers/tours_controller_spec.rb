e19cc7c1bf21baa7ad22e85d304219e1be2caa12
Leigh Passamano <leighayna.josele@gmail.com>
2018-08-08 15:19:45 -0400

    Add params to test

--- spec/controllers/tours_controller_spec.rb
+++ spec/controllers/tours_controller_spec.rb
@@ -3,19 +3,27 @@ require 'rails_helper'
 RSpec.describe ToursController, type: :controller do
   let(:user) { create :staff_user }
 
+  let :valid_params do
+    {
+      tour: {
+        title: "Awesome Tour",
+        staff_user_id: user.id,
+        cultural_center_id: user.cultural_center.id
+      }
+    }
+  end
+
+  let :invalid_params do
+    valid_params.deep_merge(tour: { title: "" })
+  end
+
   context "User authenticated" do
     before(:each) do
       authenticate_staff_user(user)
     end
 
     it 'creates a tour associated with the current user and their cultural center when provided valid data' do
-      post :create, params: {
-        tour: {
-          title: "Awesome Tour",
-          staff_user_id: user.id,
-          cultural_center_id: user.cultural_center.id
-        }
-      }
+      post :create, params: valid_params
       tour = Tour.all.last
 
       expect(response.status).to eq(200)
@@ -25,30 +33,14 @@ RSpec.describe ToursController, type: :controller do
     end
 
     it 'does not create a tour when provided invalid data' do
-      expect {
-        post :create, params: {
-          tour: {
-            title: "",
-            staff_user_id: user.id,
-            cultural_center_id: user.cultural_center.id
-          }
-        }
-      }.to_not change(Tour, :count)
+      expect { post :create, params: invalid_params }.to_not change(Tour, :count)
       expect(response.status).to eq(422)
     end
   end
 
   context "User not authenticated" do
     it 'tour can only be created if a staff user is logged in' do
-      expect {
-        post :create, params: {
-          tour: {
-            title: "Awesome Tour",
-            staff_user_id: user.id,
-            cultural_center_id: user.cultural_center.id
-          }
-        }
-      }.to_not change(Tour, :count)
+      expect { post :create, params: valid_params }.to_not change(Tour, :count)
       expect(response.status).to eq(401)
     end
   end
