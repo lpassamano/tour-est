56819b44d2306a2cb2208de354db1f5f90793689
Leigh Passamano <leighayna.josele@gmail.com>
2018-08-13 14:44:55 -0400

    Add handler for updating point information

--- app/javascript/packs/application/components/AddPointForm.js
+++ app/javascript/packs/application/components/AddPointForm.js
@@ -1,31 +1,47 @@
 import React, { Component } from "react";
 
-const AddPointForm = props => (
-  <div>
-    <p>Upload Image (WIP)</p>
-    <label htmlFor="caption">Caption: </label>
-    <textarea name="caption" id="caption" type="text" />
-  </div>
-);
+// const AddPointForm = props => (
+//   <div>
+//     <p>Upload Image (WIP)</p>
+//     <label htmlFor="caption">Caption: </label>
+//     <textarea
+//       name="caption"
+//       id="caption"
+//       type="text"
+//       onChange={props.onChange}
+//     />
+//   </div>
+// );
 
-// class AddPointForm extends Component {
-//   render() {
-//     // return form fields only for a point
-//     // div w/
-//     // state is stored here and passed to parent component?
-//     // which means handleChange also needs to happen there??
-//     return (
-//       <div>
-//         <p>Upload Image (WIP)</p>
-//         <label htmlFor="caption">Caption: </label>
-//         <textarea
-//           name="caption"
-//           id="caption"
-//           type="text"
-//         />
-//       </div>
-//     )
-//   }
-// }
+class AddPointForm extends Component {
+  state = {
+    caption: ""
+  };
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+    this.props.handlePointChange(this.state, this.props.index);
+  };
+
+  render() {
+    // return form fields only for a point
+    // div w/
+    // state is stored here and passed to parent component?
+    // which means handleChange also needs to happen there??
+    return (
+      <div id={this.props.index}>
+        <p>Upload Image (WIP)</p>
+        <label htmlFor="caption">Caption: </label>
+        <textarea
+          name="caption"
+          id="caption"
+          type="text"
+          onChange={this.handleChange}
+        />
+      </div>
+    );
+  }
+}
 
 export default AddPointForm;
