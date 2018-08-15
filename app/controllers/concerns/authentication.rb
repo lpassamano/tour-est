168b07af6bf0ce8fea083a622601f1367385742e
Leigh Passamano <leighayna.josele@gmail.com>
2018-08-15 11:48:51 -0400

    Add error to authentication and removed from app logic

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -2,6 +2,10 @@ module Authentication
   extend ActiveSupport::Concern
   included do
     helper_method :current_staff_user
+
+    rescue_from Token::InvalidError do |error|
+      render status: 401, json: { error: error.message }
+    end
   end
 
   def current_staff_user
--- app/services/token.rb
+++ app/services/token.rb
@@ -1,6 +1,9 @@
 module Token
   extend self
 
+  class InvalidError < StandardError
+  end
+
   def encode(user, expires_in: 2.hours.from_now)
     payload = { id: user.id, exp: expires_in.to_i }
     JWT.encode(payload, secret)
@@ -10,6 +13,8 @@ module Token
     jwt = JWT.decode(token, secret)[0]
     # use fetch so if id doesn't exit then it would return nil, will now raise an error instead
     StaffUser.find(jwt.fetch("id"))
+  rescue JWT::DecodeError, ActiveRecord::RecordNotFound
+    raise InvalidError, "Not authenticated"
   end
 
   private
