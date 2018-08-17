78b44009b5f8980efea6529a9276c021018fe82e
Leigh Passamano <leighayna.josele@gmail.com>
2018-08-17 09:40:59 -0400

    Add tests for redux actions - WIP

--- app/javascript/packs/application/redux/tours/actions.test.js
+++ app/javascript/packs/application/redux/tours/actions.test.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import { listTours } from "./actions";
+import { listTours, createTour, getTour } from "./actions";
 import api from "../../api";
 
 describe("listTours()", () => {
-  it("works", async () => {
+  it("calls the correct dispatch and returns data when successful", async () => {
     const dispatch = jest.fn();
     const tour = { id: 1, title: "Test Tour" };
 
@@ -19,6 +19,86 @@ describe("listTours()", () => {
     expect(dispatch).toHaveBeenCalledWith({
       type: "LIST_TOURS_SUCCESS",
       data: { [tour.id]: tour }
+    });
+  });
+
+  it("calls the correct dispatach when unsuccessful", async () => {
+    const dispatch = jest.fn();
+
+    jest.spyOn(api, "listTours").mockResolvedValue({
+      ok: false
+    });
+
+    await listTours()(dispatch);
+
+    expect(dispatch).toHaveBeenCalledWith({
+      type: "LIST_TOURS_ERROR"
+    });
+  });
+});
+
+describe("createTour()", () => {
+  it("calls the correct dispatch and returns data when successful", async () => {
+    const dispatch = jest.fn();
+    const tour = { title: "New Tour!" };
+
+    jest.spyOn(api, "createTour").mockResolvedValue({
+      ok: true,
+      data: [tour]
+    });
+
+    await createTour()(dispatch);
+
+    expect(dispatch).toHaveBeenCalledWith({
+      type: "CREATE_TOUR_SUCCESS",
+      data: tour
+    });
+  });
+
+  it("calls the correct dispatach when unsuccessful", async () => {
+    const dispatch = jest.fn();
+
+    jest.spyOn(api, "createTour").mockResolvedValue({
+      ok: false
+    });
+
+    await createTour()(dispatch);
+
+    expect(dispatch).toHaveBeenCalledWith({
+      type: "CREATE_TOUR_ERROR"
+    });
+  });
+});
+
+describe("getTour()", () => {
+  it("calls the correct dispatch and returns data when successful.", async () => {
+    const dispatch = jest.fn();
+    const tour = { id: 1, title: "Your Tour" };
+
+    jest.spyOn(api, "getTour").mockResolvedValue({
+      ok: true,
+      data: [tour]
+    });
+
+    await getTour()(dispatch);
+
+    expect(dispatch).toHaveBeenCalledWith({
+      type: "GET_TOUR_SUCCESS",
+      data: { [tour.id]: tour }
+    });
+  });
+
+  it("calls the correct dispatach when unsuccessful", async () => {
+    const dispatch = jest.fn();
+
+    jest.spyOn(api, "getTour").mockResolvedValue({
+      ok: false
+    });
+
+    await getTour()(dispatch);
+
+    expect(dispatch).toHaveBeenCalledWith({
+      type: "GET_TOUR_ERROR"
     });
   });
 });
