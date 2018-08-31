128f013dbaf2fff076f33b8ea86d13977a641317
Leigh Passamano <leighayna.josele@gmail.com>
2018-08-31 14:07:08 -0400

    Add spec for uploading image for point

--- spec/controllers/points_controller_spec.rb
+++ spec/controllers/points_controller_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe PointsController, type: :controller do
+  let(:json) { JSON.parse(response.body) }
 
+  let(:tour) { create :tour }
+
+  let(:user) { create :staff_user }
+
+  before do
+    authenticate_staff_user(user)
+  end
+
+  let :params do
+    {
+      caption: "Melancholia by Durer",
+      image: fixture_file_upload("melancholia.jpeg")
+    }
+  end
+
+  it 'creates a point with an image' do
+    post :create, params: { tour_id: tour.id, point: params, format: :json }
+    expect(response.status).to eq(200)
+
+    point = Point.find(json['id'])
+    expect(point.image).to be_attached
+  end
 end
