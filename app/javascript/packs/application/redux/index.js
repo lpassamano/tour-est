da7088a88aa2f9ec79d18021e82cdd800884cfa5
Leigh Passamano <leighayna.josele@gmail.com>
2018-09-21 13:32:02 -0400

    Add chrome redux dev tools

--- app/javascript/packs/application/redux/index.js
+++ app/javascript/packs/application/redux/index.js
@@ -1,8 +1,13 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import { reducer as staffUser } from "./staffUser";
 import { reducer as tours } from "./tours";
 import { reducer as points } from "./points";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const rootReducer = combineReducers({ staffUser, tours, points });
-export default createStore(rootReducer, applyMiddleware(thunk));
+export default createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
